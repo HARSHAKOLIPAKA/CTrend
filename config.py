b4ec0181d4ff2aab05d8677985b4463469626403
HARSHAKOLIPAKA <harshavardhankolipaka5@gmail.com>
2026-05-29 15:07:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from sockets import socketio
+from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+socketio.init_app(app)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)

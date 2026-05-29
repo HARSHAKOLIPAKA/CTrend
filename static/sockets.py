ae92ef910ec5ee1e80f833496cbbea41a4358765
HARSHAKOLIPAKA <harshavardhankolipaka5@gmail.com>
2026-05-29 15:05:41 +0530

    Add SocketIO support for messaging and connections

--- static/sockets.py
+++ static/sockets.py
@@ -1,1 +1,16 @@
+from flask_socketio import SocketIO, emit, join_room
 
+socketio = SocketIO(cors_allowed_origins="*")
+
+@socketio.on("connect")
+def connect():
+    print("user connected")
+
+@socketio.on("message")
+def handle_message(data):
+    to = data["to"]
+    msg = data["msg"]
+
+    emit("message", {
+        "msg": msg
+    }, room=to)
